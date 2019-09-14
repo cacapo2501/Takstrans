44668c43d57ff24ec74e7e2e67865004ea87b436
cacapo2501@gmail.com <cacapo2501@gmail.com>
2019-09-14 21:24:51 +0900

    コンパイルバッチを修正

--- [[pak]]libraries/custom/tokyu/compile120.4.bat
+++ [[pak]]libraries/custom/tokyu/compile120.4.bat
@@ -1,3 +1,7 @@
 @echo off
 makeobj62-0 pak Tokyu.pak 2020.dat 5000.dat 8500.dat 9000.dat > err.txt
+makeobj62-0 pak Tokyu2020.pak 2020.dat >> err.txt
+makeobj62-0 pak Tokyu5000.pak 5000.dat >> err.txt
+makeobj62-0 pak Tokyu8500.pak 8500.dat >> err.txt
+makeobj62-0 pak Tokyu9000.pak 9000.dat >> err.txt
 pause
