8fed803457f05d4f933aab863b8134a1a16a4f7d
cacapo2501@gmail.com <cacapo2501@gmail.com>
2019-10-05 19:53:32 +0900

    東急6000系を追加

--- [[pak]]libraries/custom/tokyu/compile120.4.bat
+++ [[pak]]libraries/custom/tokyu/compile120.4.bat
@@ -1,8 +1,9 @@
 @echo off
-makeobj62-0 pak Tokyu.pak 2000.dat 2020.dat 5000.dat 8500.dat 9000.dat > err.txt
+makeobj62-0 pak Tokyu.pak 2000.dat 2020.dat 5000.dat 6000.dat 8500.dat 9000.dat > err.txt
 makeobj62-0 pak Tokyu2000.pak 2000.dat >> err.txt
 makeobj62-0 pak Tokyu2020.pak 2020.dat >> err.txt
 makeobj62-0 pak Tokyu5000.pak 5000.dat >> err.txt
+makeobj62-0 pak Tokyu6000.pak 6000.dat >> err.txt
 makeobj62-0 pak Tokyu8500.pak 8500.dat >> err.txt
 makeobj62-0 pak Tokyu9000.pak 9000.dat >> err.txt
 pause
