efba86cf1cc4dcf03f801da91a27beb16392f3a1
cacapo2501@gmail.com <cacapo2501@gmail.com>
2019-11-03 22:15:35 +0900

    跨座式モノレール軌道を追加中

--- [[pak]]libraries/RBDTRail/compile_real120.4.bat
+++ [[pak]]libraries/RBDTRail/compile_real120.4.bat
@@ -5,12 +5,14 @@ del err.txt
 rem pak‰»‚·‚é
 makeobj62-0 pak96 RBDT_Wire.pak early_power_wire.dat >> err.txt
 makeobj62-0 pak RBDT_Track.pak early_ballast_track.dat modern_ballast_track.dat >> err.txt
+makeobj62-0 pak RBDT_Monorail.pak monorail_way.dat >> err.txt
 
 rem ƒ}[ƒW‚·‚é
-makeobj62-0 merge RBDTset.pak RBDT_Track.pak RBDT_Wire.pak >> err.txt
+makeobj62-0 merge RBDTset.pak RBDT_Track.pak RBDT_Wire.pak RBDT_Monorail.pak >> err.txt
 
 rem ŒÂ•Êpak‚ğíœ‚·‚é
 del RBDT_Track.pak
 del RBDT_Wire.pak
+del monorail_way.pak
 
 pause
