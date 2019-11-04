d804c3f64a28064301fa607265885ff3e98cf714
cacapo2501@gmail.com <cacapo2501@gmail.com>
2019-11-04 20:50:44 +0900

    モノレール軌道、東京モノレールを追加

--- [[pak]]libraries/RBDTRail/compile_real120.4.bat
+++ [[pak]]libraries/RBDTRail/compile_real120.4.bat
@@ -4,15 +4,17 @@ del err.txt
 
 rem pak‰»‚·‚é
 makeobj62-0 pak96 RBDT_Wire.pak early_power_wire.dat >> err.txt
+makeobj62-0 pak96 RBDT_MonorailPillar.pak monorail_pillar.dat >> err.txt
 makeobj62-0 pak RBDT_Track.pak early_ballast_track.dat modern_ballast_track.dat >> err.txt
 makeobj62-0 pak RBDT_Monorail.pak monorail_way.dat >> err.txt
 
 rem ƒ}[ƒW‚·‚é
-makeobj62-0 merge RBDTset.pak RBDT_Track.pak RBDT_Wire.pak RBDT_Monorail.pak >> err.txt
+makeobj62-0 merge RBDTset.pak RBDT_Track.pak RBDT_Wire.pak RBDT_Monorail.pak RBDT_MonorailPillar.pak >> err.txt
 
 rem ŒÂ•Êpak‚ğíœ‚·‚é
 del RBDT_Track.pak
 del RBDT_Wire.pak
 del RBDT_Monorail.pak
+del RBDT_MonorailPillar.pak
 
 pause
