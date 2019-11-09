250c7b3faa29b9fa2677b780ce35bdd1e428297f
cacapo2501@gmail.com <cacapo2501@gmail.com>
2019-11-09 18:16:07 +0900

    東京モノレール第３次塗装を追加

--- [[pak]]libraries/custom/tokyo_monorail/compile120.4.bat
+++ [[pak]]libraries/custom/tokyo_monorail/compile120.4.bat
@@ -1,3 +1,3 @@
 @echo off
-makeobj62-0 pak TokyoMonorail.pak 300.dat 500.dat 1000.dat 1000_2.dat > err.txt
+makeobj62-0 pak TokyoMonorail.pak 300.dat 500.dat 1000.dat 1000_2.dat 1000_3.dat > err.txt
 pause
