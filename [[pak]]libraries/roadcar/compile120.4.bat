b85f4ed870b6c944f382692681122db1f7e5abac
cacapo2501@gmail.com <cacapo2501@gmail.com>
2019-12-11 20:37:27 +0900

    トヨタスープラ(2019年型)を追加

--- [[pak]]libraries/roadcar/compile120.4.bat
+++ [[pak]]libraries/roadcar/compile120.4.bat
@@ -1,4 +1,5 @@
 @echo off
 makeobj62-0 pak bus_base.pak bus_base.dat > err.txt
 makeobj62-0 pak erga_duo.pak isuzu_erga_duo.dat > err.txt
+makeobj62-0 pak supra_2019.pak supra_2019.dat > err.txt
 pause
