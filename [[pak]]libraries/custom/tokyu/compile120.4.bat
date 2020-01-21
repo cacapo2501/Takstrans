90d94b91ec2d1b2e33eadd3a71fff3703012cc72
cacapo2501 <cacapo2501@gmail.com>
2020-01-22 06:49:55 +0900

    東急6020系を追加

--- [[pak]]libraries/custom/tokyu/compile120.4.bat
+++ [[pak]]libraries/custom/tokyu/compile120.4.bat
@@ -5,6 +5,7 @@ makeobj62-0 pak Tokyu2020.pak 2020.dat >> err.txt
 makeobj62-0 pak Tokyu3020.pak 3020.dat >> err.txt
 makeobj62-0 pak Tokyu5000.pak 5000.dat >> err.txt
 makeobj62-0 pak Tokyu6000.pak 6000.dat >> err.txt
+makeobj62-0 pak Tokyu6020.pak 6020.dat >> err.txt
 makeobj62-0 pak Tokyu8500.pak 8500.dat >> err.txt
 makeobj62-0 pak Tokyu9000.pak 9000.dat >> err.txt
 pause
