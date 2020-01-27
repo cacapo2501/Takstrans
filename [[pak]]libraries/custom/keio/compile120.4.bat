53ef4b9a2f375e058c6bca8bea88796df2d41571
cacapo2501 <cacapo2501@gmail.com>
2020-01-27 21:02:10 +0900

    京王8000系を追加中

--- [[pak]]libraries/custom/keio/compile120.4.bat
+++ [[pak]]libraries/custom/keio/compile120.4.bat
@@ -1,4 +1,5 @@
 @echo off
 makeobj62-0 pak Keio5000ii.pak 5000ii.dat > err.txt
-makeobj62-0 pak Keio9000.pak 9000.dat > err.txt
+makeobj62-0 pak Keio9000.pak 9000.dat >> err.txt
+makeobj62-0 pak Keio8000.pak 8000.dat >> err.txt
 pause
