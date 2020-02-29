ac59e42ef3658eeaf039f513835d8b054fb64830
cacapo2501@gmail.com <cacapo2501@gmail.com>
2020-02-29 15:16:36 +0900

    JHTRを更新中

---
+++ sdmpy/sdmmain.py
@@ -0,0 +1,11 @@
+import os
+
+def display_main_menu():
+    print(u"1:新しいプロジェクトを開く\n")
+    print(u"2:既存のプロジェクトを開く\n")
+    print(u"3:終了する\n")
+    input(u"処理を選択してください:")
+
+print(u"SDMPY!へようこそ")
+
+display_main_menu()
