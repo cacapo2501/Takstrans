cd5e484907c75da9cc38fc7b4c1e795231cf33d5
cacapo2501@gmail.com <cacapo2501@gmail.com>
2020-03-01 05:52:15 +0900

    JRW700系7000番台を追加 二車線道路を修正

--- [[pak]]libraries/JHTR/compile120.4.bat
+++ [[pak]]libraries/JHTR/compile120.4.bat
@@ -1,3 +1,3 @@
 @echo off
-makeobj62-0 pak JHTR.pak baseroad.dat > err.txt
+makeobj62-0 pak JHTR.pak dual_to_dual_highway.dat dual_to_signle_highway.dat single_lane_highway.dat > err.txt
 pause
--- [[pak]]libraries/custom/jr_jnr/700/compile120.4.bat
+++ [[pak]]libraries/custom/jr_jnr/700/compile120.4.bat
@@ -1,3 +1,4 @@
 @echo off
 makeobj62-0 pak jrc700.pak 700.dat > err.txt
+makeobj62-0 pak jrw700-7000.pak 700-7000.dat > err.txt
 pause
