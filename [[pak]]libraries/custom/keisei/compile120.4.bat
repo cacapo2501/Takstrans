669f792c566eafdce032b903892a2d38e940b7fe
cacapo2501 <cacapo2501@gmail.com>
2020-03-17 19:12:26 +0900

    AE100系を追加

--- [[pak]]libraries/custom/keisei/compile120.4.bat
+++ [[pak]]libraries/custom/keisei/compile120.4.bat
@@ -1,4 +1,5 @@
 @echo off
 makeobj62-0 pak Keisei3100ii.pak 3100ii.dat > err.txt
-makeobj62-0 pak Keiseiae.pak ae.dat > err.txt
+makeobj62-0 pak Keiseiae.pak ae.dat ae_renewed,dat >> err.txt
+makeobj62-0 pak Keiseiae100.pak ae100.dat >> err.txt
 pause
