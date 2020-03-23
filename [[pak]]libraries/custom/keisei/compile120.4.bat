785c23a5fdf3e1585c03be019631d00d0f1be7fd
cacapo2501 <cacapo2501@gmail.com>
2020-03-23 20:55:28 +0900

    AE(II)を追加

--- [[pak]]libraries/custom/keisei/compile120.4.bat
+++ [[pak]]libraries/custom/keisei/compile120.4.bat
@@ -2,4 +2,5 @@
 makeobj62-0 pak Keisei3100ii.pak 3100ii.dat > err.txt
 makeobj62-0 pak Keiseiae.pak ae.dat ae_renewed,dat >> err.txt
 makeobj62-0 pak Keiseiae100.pak ae100.dat >> err.txt
+makeobj62-0 pak Keiseiaeii.pak aeii.dat >> err.txt
 pause
