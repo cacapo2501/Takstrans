37e873f64dfe185af8e47d921ced12f05c5a1ad0
cacapo2501 <cacapo2501@gmail.com>
2020-05-17 16:10:06 +0900

    USRail EMD F7/F9を追加

--- [[pak]]libraries/box_building/compile120.4.bat
+++ [[pak]]libraries/box_building/compile120.4.bat
@@ -1,3 +1,5 @@
 @echo off
 makeobj62-0 pak box_buidling_brick.pak box_buidling_brick.dat > err.txt
+makeobj62-0 pak box_buidling_overpass.pak box_buidling_overpass.dat > err.txt
+makeobj62-0 pak96 box_buidling_overpass_step.pak box_buidling_overpass_step.dat > err.txt
 pause
---
+++ [[pak]]libraries/custom/usrail/emd_f9/compile120.4.bat
@@ -0,0 +1,3 @@
+@echo off
+makeobj60-3 pak emd_f9.pak f9.dat > err.txt
+pause
