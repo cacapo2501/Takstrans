9e9ed66ce876df7fe79ce7337e0e5e3a11931f1a
cacapo2501 <cacapo2501@gmail.com>
2020-08-02 21:13:16 +0900

    相鉄8000系を追加

--- [[pak]]libraries/custom/sotetsu/compile120.4.bat
+++ [[pak]]libraries/custom/sotetsu/compile120.4.bat
@@ -1,4 +1,5 @@
 @echo off
-makeobj62-0 pak sotetsu20000.pak 20000.dat > err.txt
-makeobj62-0 pak sotetsu12000.pak 12000.dat > err.txt
+makeobj62-0 pak sotetsu8000.pak 8000_1st.dat 8000_2nd.dat 8000_3rd.dat > err.txt
+makeobj62-0 pak sotetsu20000.pak 20000.dat >> err.txt
+makeobj62-0 pak sotetsu12000.pak 12000.dat >> err.txt
 pause
