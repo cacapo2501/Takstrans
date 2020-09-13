77404e6bc2194b4456a0f3d762632fc4242a7a6b
cacapo2501 <cacapo2501@gmail.com>
2020-09-13 19:25:39 +0900

    地方駅舎を追加

--- [[pak]]libraries/box_station/compile120.4.bat
+++ [[pak]]libraries/box_station/compile120.4.bat
@@ -1,4 +1,5 @@
 @echo off
 makeobj62-0 pak showa_era_station_buidling.pak showa_station_building.dat > err.txt
+makeobj62-0 pak old_showa_era_station_house.pak old_station_house.dat >> err.txt
 makeobj62-0 pak pedestrian_bridge.pak pedestrian_bridge.dat >> err.txt
 pause
