3d8fd3ca870d1afff985f75777fca0e388ff5828
cacapo2501 <cacapo2501@gmail.com>
2020-09-13 20:22:50 +0900

    日産セドリックタクシーを追加

--- [[pak]]libraries/roadcar/compile120.4.bat
+++ [[pak]]libraries/roadcar/compile120.4.bat
@@ -1,5 +1,6 @@
 @echo off
 makeobj62-0 pak bus_base.pak bus_base.dat > err.txt
-makeobj62-0 pak erga_duo.pak isuzu_erga_duo.dat > err.txt
-makeobj62-0 pak supra_2019.pak supra_2019.dat > err.txt
+makeobj62-0 pak erga_duo.pak isuzu_erga_duo.dat >> err.txt
+makeobj62-0 pak supra_2019.pak supra_2019.dat >> err.txt
+makeobj62-0 pak cedric_taxi.pak cedric_taxi.dat >> err.txt
 pause
