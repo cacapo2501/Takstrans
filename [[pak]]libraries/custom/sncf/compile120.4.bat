3ffd971563b595d9315f49f6d9d878af79dde16c
cacapo2501 <cacapo2501@gmail.com>
2020-10-05 21:13:14 +0900

    BB36000を追加

--- [[pak]]libraries/custom/sncf/compile120.4.bat
+++ [[pak]]libraries/custom/sncf/compile120.4.bat
@@ -3,4 +3,5 @@ makeobj62-0 pak sncf_bb9200.pak bb9200.dat > err.txt
 makeobj62-0 pak sncf_bb8500.pak bb8500.dat >> err.txt
 makeobj62-0 pak sncf_bb7200.pak bb7200.dat >> err.txt
 makeobj62-0 pak sncf_bb26000.pak bb26000.dat >> err.txt
+makeobj62-0 pak sncf_bb36000.pak bb36000.dat >> err.txt
 pause
