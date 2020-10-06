f683d7995e0a4b98fc7aa4ae32f8931d2c23289c
cacapo2501 <cacapo2501@gmail.com>
2020-10-07 04:15:43 +0900

    BB27000を追加

--- [[pak]]libraries/custom/sncf/compile120.4.bat
+++ [[pak]]libraries/custom/sncf/compile120.4.bat
@@ -4,4 +4,5 @@ makeobj62-0 pak sncf_bb8500.pak bb8500.dat >> err.txt
 makeobj62-0 pak sncf_bb7200.pak bb7200.dat >> err.txt
 makeobj62-0 pak sncf_bb26000.pak bb26000.dat >> err.txt
 makeobj62-0 pak sncf_bb36000.pak bb36000.dat >> err.txt
+makeobj62-0 pak sncf_bb27000.pak bb27000.dat >> err.txt
 pause
