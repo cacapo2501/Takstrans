fad04445b5f66a027aea8f46d469ccd3e9f816e1
cacapo2501 <cacapo2501@gmail.com>
2020-10-11 18:12:09 +0900

    Revert "日野ブルーリボンIIを追加"
    This reverts commit 5d020dc650119ed2bee408e6f132fd1f3cdd94c3.

--- [[pak]]libraries/roadcar/compile120.4.bat
+++ [[pak]]libraries/roadcar/compile120.4.bat
@@ -1,7 +1,6 @@
 @echo off
 makeobj62-0 pak bus_base.pak bus_base.dat > err.txt
 makeobj62-0 pak erga_duo.pak isuzu_erga_duo.dat >> err.txt
-makeobj62-0 pak hino_blue_ribbon_ii.pak hino_blue_ribbon_ii.dat >> err.txt
 makeobj62-0 pak supra_2019.pak supra_2019.dat >> err.txt
 makeobj62-0 pak cedric_taxi.pak cedric_taxi.dat >> err.txt
 pause
