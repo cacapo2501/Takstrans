40d3f783b1f1b3654c1933b2a664c469e6c36e77
cacapo2501 <cacapo2501@gmail.com>
2020-11-24 18:48:08 +0900

    NSX(NA1)とMR2を追加

--- [[pak]]libraries/roadcar/compile120.4.bat
+++ [[pak]]libraries/roadcar/compile120.4.bat
@@ -8,4 +8,6 @@ makeobj62-0 pak mazda_mpv_lv.pak mazda_mpv_lv.dat >> err.txt
 makeobj62-0 pak honda_fit.pak honda_fit.dat >> err.txt
 makeobj62-0 pak suzuki_alto.pak suzuki_alto.dat >> err.txt
 makeobj62-0 pak cedric_taxi.pak cedric_taxi.dat >> err.txt
+makeobj62-0 pak honda_nsx_na1.pak honda_nsx_na1.dat >> err.txt
+makeobj62-0 pak toyota_mr2.pak toyota_mr2.dat >> err.txt
 pause
