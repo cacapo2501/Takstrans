357dd0f910a777a27d75afb967097b1517f037e1
cacapo2501 <cacapo2501@gmail.com>
2021-01-02 20:56:58 +0900

    西鉄3000系を追加

--- [[pak]]libraries/buildings/compile120.4.bat
+++ [[pak]]libraries/buildings/compile120.4.bat
@@ -1,4 +1,5 @@
 @echo off
 makeobj62-0 pak super_market.pak super_market1.dat > err.txt
 makeobj62-0 pak small_apartment.pak small_apartment.dat >> err.txt
+makeobj62-0 pak commercial_tower_2x2.pak commercial_tower_2x2.dat >> err.txt
 pause
---
+++ [[pak]]libraries/custom/nishitetsu/compile120.4.bat
@@ -0,0 +1,3 @@
+@echo off
+makeobj62-0 pak nishitetsu3000.pak 3000.dat
+pause
