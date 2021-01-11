5914be57acedbc38f996296eeec59581b401e67a
cacapo2501 <cacapo2501@gmail.com>
2021-01-11 14:32:32 +0900

    西鉄7000系・7050系を追加

--- [[pak]]libraries/custom/nishitetsu/compile120.4.bat
+++ [[pak]]libraries/custom/nishitetsu/compile120.4.bat
@@ -2,4 +2,6 @@
 makeobj62-0 pak nishitetsu3000.pak 3000.dat
 makeobj62-0 pak nishitetsu5000.pak 5000.dat
 makeobj62-0 pak nishitetsu6000.pak 6000.dat
+makeobj62-0 pak nishitetsu7000.pak 7000.dat
+makeobj62-0 pak nishitetsu7050.pak 7050.dat
 pause
