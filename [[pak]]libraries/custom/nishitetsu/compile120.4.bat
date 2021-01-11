2616baad22772c10401164ec3dc294f8ac0f3500
cacapo2501 <cacapo2501@gmail.com>
2021-01-11 18:22:52 +0900

    西鉄9000系を追加

--- [[pak]]libraries/custom/nishitetsu/compile120.4.bat
+++ [[pak]]libraries/custom/nishitetsu/compile120.4.bat
@@ -4,4 +4,5 @@ makeobj62-0 pak nishitetsu5000.pak 5000.dat
 makeobj62-0 pak nishitetsu6000.pak 6000.dat
 makeobj62-0 pak nishitetsu7000.pak 7000.dat
 makeobj62-0 pak nishitetsu7050.pak 7050.dat
+makeobj62-0 pak nishitetsu9000.pak 9000.dat
 pause
