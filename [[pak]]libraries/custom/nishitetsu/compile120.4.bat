e29cab528a0528b26bc4dc143c6b90b1ea52847b
cacapo2501 <cacapo2501@gmail.com>
2021-01-16 05:43:34 +0900

    西鉄2000系を追加

--- [[pak]]libraries/custom/nishitetsu/compile120.4.bat
+++ [[pak]]libraries/custom/nishitetsu/compile120.4.bat
@@ -1,4 +1,5 @@
 @echo off
+makeobj62-0 pak nishitetsu2000.pak 2000.dat
 makeobj62-0 pak nishitetsu3000.pak 3000.dat
 makeobj62-0 pak nishitetsu5000.pak 5000.dat
 makeobj62-0 pak nishitetsu6000.pak 6000.dat
