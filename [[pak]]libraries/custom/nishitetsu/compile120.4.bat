b240e0577849dc1b62f6c1b10f58b763845c51ea
cacapo2501 <cacapo2501@gmail.com>
2021-03-19 21:14:00 +0900

    西鉄1000形を追加

--- [[pak]]libraries/custom/nishitetsu/compile120.4.bat
+++ [[pak]]libraries/custom/nishitetsu/compile120.4.bat
@@ -1,4 +1,5 @@
 @echo off
+makeobj62-0 pak nishitetsu1000.pak 1000.dat
 makeobj62-0 pak nishitetsu2000.pak 2000.dat
 makeobj62-0 pak nishitetsu3000.pak 3000.dat
 makeobj62-0 pak nishitetsu5000.pak 5000.dat
