14527aa6b6bf55182a512a6e4ae109a0903bdb61
cacapo2501 <cacapo2501@gmail.com>
2021-06-26 17:24:43 +0900

    京急を修正

--- [[pak]]libraries/custom/keikyu/compile120.4.bat
+++ [[pak]]libraries/custom/keikyu/compile120.4.bat
@@ -1,3 +1,3 @@
 @echo off
-makeobj62-0 pak Tak_Keikyu.pak 1000ii.dat 800ii.dat 600ii.dat 500.dat 700ii.dat 2000.dat 1500.dat 1000.dat 600iii.dat > err.txt
+makeobj62-0 pak Tak_Keikyu.pak 1000ii.dat 800ii.dat 600ii.dat 500.dat 700ii.dat 2000.dat 1500.dat 1000.dat 600iii.dat 2100.dat 1000ii_1890.dat 1000ii_stainless.dat 1000ii_yellow.dat > err.txt
 pause
