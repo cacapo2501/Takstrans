b0a8bb425a1be8940dfd46c81090e95b76e8fba3
cacapo2501 <cacapo2501@gmail.com>
2021-07-21 19:07:32 +0900

    東京メトロ18000を修正

--- [[pak128org]]libraries/trains/compile120.4.bat
+++ [[pak128org]]libraries/trains/compile120.4.bat
@@ -2,4 +2,5 @@
 makeobj62-0 pak128 209.pak 209.dat > err.txt
 makeobj62-0 pak128 jre_e2.pak jre_e2.dat jre_e2_1000.dat >> err.txt
 makeobj62-0 pak128 jre_e3.pak jre_e3.dat jre_e3_1000.dat jre_e3_1000_renewed.dat >> err.txt
+makeobj62-0 pak128 tokyometro_18000.pak tokyometro_18000.dat >> err.txt
 pause
